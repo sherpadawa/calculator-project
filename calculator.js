7bdd251d673647aa8c5017f86ec275a07c0b3baa
sherpadawa <dawa.sherpa@mihsnyc.org>
2016-01-25 14:32:23 +0000

    added functions to my calculator, now it divides, multiplies, substracts, adds etc.

--- calculator.js
+++ calculator.js
@@ -118,7 +118,7 @@ $(document).ready(function()
 				else if (operator == "+")
 					answer = add(parseInt(first_operand), parseInt(second_operand));	
 				else if (operator == "^")
-					answer = power(parseInt(first_operand), parseInt(second_operand));	
+					answer = power (parseInt(first_operand), parseInt(second_operand));	
 
 				$('#question').append(" =");
 				$('#answer').append(answer).hide().fadeIn(1000);
@@ -132,5 +132,23 @@ $(document).ready(function()
 function divide(x,y)
 {
   return x/y;
+}
+function add(x,y){
+	return x+y;
+}
+function subtract(x,y){
+	return x-y;
+}
+function multiply(x,y){
+	return x*y;
+}
+function square_root(x){
+	return Math.sqrt(x);
+}
+function logBase10(x){
+	return Math.log10(x);
+}
+function generateRandomNumber(){
+	return Math.random();
 }
 // You must implement the rest of the functions here.
